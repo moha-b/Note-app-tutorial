6410b9e3f041d610747b661249ac2363bd316796
moha-b <20193015@fue.edu.eg>
2022-05-20 16:21:18 +0200

    update

--- app/src/main/java/com/example/noteapp/logIn.java
+++ app/src/main/java/com/example/noteapp/logIn.java
@@ -1,20 +1,42 @@
 package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class logIn extends AppCompatActivity {
 
-    // variable section
-    EditText userName = findViewById(R.id.logInUserName);
-    EditText password = findViewById(R.id.logInPassword);
-    Button go = findViewById(R.id.Login);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        // variable section
+        EditText userName = findViewById(R.id.logInUserName);
+        EditText password = findViewById(R.id.logInPassword);
+        Button go = findViewById(R.id.Login);
+
+        // go to home page
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user = userName.getText().toString();
+                String userPassword = password.getText().toString();
+
+//                if (user.isEmpty() || userPassword.isEmpty()){
+//                    Toast.makeText(getApplicationContext(),"lol",Toast.LENGTH_SHORT).show();
+//                }else{
+//                    connection with SQLite
+//                }
+
+                Intent intent = new Intent(logIn.this,Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/noteapp/splash_screen.java
@@ -0,0 +1,19 @@
+package com.example.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+//        new Handler().postDelayed(Runnable(){
+//
+//        });
+    }
+}
